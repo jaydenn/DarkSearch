72371ffd53f8f64a6185634428197ed00d936783
Jayden Newstead <jnewstea@asu.edu>
2015-06-08 09:29:44 -0700

    cleaned up detector fucntions file

--- source/detectorFunctions.cpp
+++ source/detectorFunctions.cpp
@@ -1,26 +1,61 @@
-#include <math.h>
-#include <stdio.h>
-double detEff(double Er, int type){
-switch( type ) {
-case 0: {return 1;}
-case 1: {return .4;}
-case 2: {return .4;}
-case 3: {return .5;}
-case 4: {return 1;}
-}printf("invalid detetector efficiency\n"); return NAN; }
-double detBackground(double Er, int type){
-switch( type ) {
-case 0: {return 1e-6;}
-case 1: {return 1e-9;}
-case 2: {return 4e-6;}
-case 3: {return 1e-5;}
-case 4: {return 1e-6;}
-}printf("invalid detetector background\n"); return NAN; }
-double detRes(double Er, int type){
-switch( type ) {
-case 0: {return 5;}
-case 1: {return 5;}
-case 2: {return 5;}
-case 3: {return 5;}
-case 4: {return 5;}
-}printf("invalid detetector resolution\n"); return NAN; }
+#include <cmath>
+#include <cstdio>
+
+double detEff(double Er, int type)
+{
+    switch( type ) 
+    {
+        case 0: 
+            return 1;
+        case 1: 
+            return .4;
+        case 2: 
+            return .4;
+        case 3: 
+            return .5;
+        case 4: 
+            return 1;
+        default:
+            printf("invalid detector efficiency\n"); 
+            return NAN;
+    }
+}
+
+double detBackground(double Er, int type)
+{
+    switch( type ) 
+    {
+        case 0: 
+            return 1e-6;
+        case 1: 
+            return 1e-9;
+        case 2: 
+            return 4e-6;
+        case 3: 
+            return 1e-5;
+        case 4: 
+            return 1e-6;
+        default:
+            printf("invalid detector background\n"); 
+            return NAN; 
+    }
+}
+double detRes(double Er, int type)
+{
+    switch( type ) 
+    {
+        case 0: 
+            return 5;
+        case 1: 
+            return 5;
+        case 2: 
+            return 5;
+        case 3: 
+            return 5;
+        case 4: 
+            return 1;
+        default:
+            printf("invalid detector resolution\n"); 
+            return NAN; 
+    }
+}
