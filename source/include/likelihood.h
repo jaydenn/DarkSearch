0fb21b98fffbe652813d501f483912c25feaa206
Jayden Newstead <jnewstea@asu.edu>
2016-02-19 15:24:34 -0700

    changes to include for mem leak fix

--- source/include/likelihood.h
+++ source/include/likelihood.h
@@ -7,10 +7,10 @@ double logPoisson(int obs, double expect);
 void scaleParams(double *Cube, reconstructionParameters P, WIMPpars W);
 
 //likelihood function for binned data
-double logLikelihood( WIMPpars W, detector *dets, int ndets, reconstructionParameters P, int b);
+double logLikelihood( WIMPpars *W, detector *dets, int ndets, reconstructionParameters P, int b);
 
 //likelihood function for unbinned data
-double logLikelihoodBinless( WIMPpars W, detector *dets, int ndets, reconstructionParameters P, int b);
+double logLikelihoodBinless( WIMPpars *W, detector *dets, int ndets, reconstructionParameters P, int b);
 
 //log likelihood function for passing the above to Multinest
 void LogLikedN(double *Cube, int &ndim, int &npars, double &lnew, long &pointer);
