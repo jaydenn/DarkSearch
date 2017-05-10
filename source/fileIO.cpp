fe458f4b7e969503e39f1d60efabfb870398d543
Jayden Newstead <jnewstea@asu.edu>
2017-05-10 14:32:59 -0700

    bug in file input

--- source/fileIO.cpp
+++ source/fileIO.cpp
@@ -287,8 +287,8 @@ int getSamplingPars(parameterList *pL, char *filename)
                 }
             }
             //include non-scanned parameter for a0 (which is fixed by normalization)
-            pL->p.vLa[aInd][3] = (double)pL->p.nDim+pL->p.nPar++;
-            pL->p.vLa[aInd][2] = 3;
+            pL->p.vLa[0][3] = (double)pL->p.nDim+pL->p.nPar++;
+            pL->p.vLa[0][2] = 3;
             sprintf(pL->p.parNames[(int)pL->p.vLa[0][3]],"a0");
         }
         ret = fgets(temp,200,input);
