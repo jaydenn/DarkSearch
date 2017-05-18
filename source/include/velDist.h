6f69fe357db30ebcf6073c5ec40b4b2e7f6cc733
Jayden Newstead <jnewstea@asu.edu>
2017-05-18 13:13:40 -0500

    forgot to add a function to header

--- source/include/velDist.h
+++ source/include/velDist.h
@@ -1,2 +1,3 @@
 double G(double v0, double ve, double vesc, double vmin, int index, double *a);
 double Gsq(double v0, double ve, double vesc, double vmin, int index, double *a);
+double fpoly(double v, double *a, int index);
